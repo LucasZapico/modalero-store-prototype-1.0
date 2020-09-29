a1531570b8e48ed5833ff6e58a5261de59b031fb
Lucas zapico <zapicolucas@gmail.com>
2020-09-29 13:21:41 -0700

    add responsive styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,8 +9,10 @@ import Collections from "./Collection";
 
 const HomeFeatured = () => {
   return (
-    <div className="home__featured section--default bg--dark padding__all--l">
-      <div className="col">
+    <div className="section--default bg--dark">
+    <div className="home__featured  padding__all--l container__content">
+    
+      <div className="col featured__content padding__bottom--l">
       <h3>Featured</h3>
       <h5 className="display-caption">The Black Mango is a bold addition to any dinning or conference  room. </h5>
       <Link className="cta margin__top--m">
@@ -20,13 +22,14 @@ const HomeFeatured = () => {
         </div>
       </Link>
       </div>
-      <div className="col">
-        <div className="featured-one ">
+      <div className="col featured__images">
+        <div className="featured-one featured__image">
 
         </div>
-        <div className="featured-two ">
+        <div className="featured-two featured__image">
           
         </div>
+      </div>
       </div>
     </div>
   )
@@ -37,7 +40,7 @@ const HomePage = () => {
     <div className="page__home">
       <HomeHero/>
       <section className="section--small">
-        <div className="margin__vert--l">
+        <div className="margin__vert--l padding__hor--l">
         <h3 className=" center">Our Philosophy</h3>
         <h5 className="center margin__top color-sec">We make furniture that are one of a kind blends of natural and modern beauty. Each piece we craft is an endeavor to celebrate function and elegance.</h5>
         
